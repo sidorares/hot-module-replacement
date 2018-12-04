22c538a686f38c3ebb56b0d4d6732196996f4bf5
Andrey Sidorov <sidorares@yandex.com>
2018-12-04 12:17:37 +1100

    Instead of monkey-patching .compile which is only called for js add hoocks one level up in extensions. Fixes #5

---
+++ test/fixtures/accept-json-children/main.js
@@ -0,0 +1,30 @@
+require('../../../index.js')();
+
+let dep = require('./dependency.json');
+
+const main = () => {
+  process.send && process.send({ message: 'call from main', param: dep.value });
+};
+
+process.send && process.send({ message: 'start' });
+main();
+
+const interval = setInterval(() => {
+  // nothing, just to have something in the event loop
+}, 1000);
+
+process.on('message', function(msg) {
+  process.exit(0);
+});
+
+if (module.hot) {
+  module.hot.accept('./dependency', function(updatedDep) {
+    dep = require('./dependency');
+    process.send &&
+      process.send({
+        message: 'call from accept handler',
+        value: dep.value
+      });
+    main();
+  });
+}
---
+++ test/integration/accept-json-children.js
@@ -0,0 +1,47 @@
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs');
+const fork = require('child_process').fork;
+
+describe('when one level JSON dependency is updated', () => {
+  describe('and parent accepts it', () => {
+    it('should call accept handler', done => {
+      const child = fork('../fixtures/accept-json-children/main.js', {
+        cwd: __dirname
+      });
+
+      let touched = false;
+      let accepted = false;
+      const depMessages = [];
+      const updatedValue = +new Date();
+
+      child.on('message', message => {
+        switch (message.message) {
+          case 'start':
+            touched = true;
+            fs.writeFileSync(
+              path.join(__dirname, '../fixtures/accept-json-children/dependency.json'),
+              JSON.stringify({ value: updatedValue })
+            );
+            break;
+          case 'vall from main':
+            assert.equal(message.value, 42);
+            break;
+
+          case 'call from accept handler':
+            if (!touched) {
+              done(new Error('accepted before dependency is changed'));
+            }
+            accepted = true;
+            child.on('exit', e => {
+              assert.equal(e, 0);
+              assert.equal(message.value, updatedValue);
+              done();
+            });
+            child.send('exit');
+            break;
+        }
+      });
+    });
+  });
+});
